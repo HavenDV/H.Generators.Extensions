efe6c35965c515af234b7aedfbaa9e0229aaeba3
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-05-18 18:35:53 +1000

    feat: Initial commit.

---
+++ src/libs/H.Generators.Extensions/AnalyzerConfigOptionsProviderExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace H.Generators.Extensions;
+
+public static class AnalyzerConfigOptionsProviderExtensions
+{
+    public static string? GetGlobalOption(
+        this AnalyzerConfigOptionsProvider provider,
+        string name)
+    {
+        provider = provider ?? throw new ArgumentNullException(nameof(provider));
+        name = name ?? throw new ArgumentNullException(nameof(name));
+
+        return provider.GlobalOptions.TryGetValue(
+            $"build_property.{name}",
+            out var result) &&
+            !string.IsNullOrWhiteSpace(result)
+            ? result
+            : null;
+    }
+
+    public static string? GetOption(
+        this AnalyzerConfigOptionsProvider provider,
+        AdditionalText text,
+        string name)
+    {
+        provider = provider ?? throw new ArgumentNullException(nameof(provider));
+        name = name ?? throw new ArgumentNullException(nameof(name));
+
+        return provider.GetOptions(text).TryGetValue(
+            $"build_metadata.AdditionalFiles.{name}",
+            out var result) &&
+            !string.IsNullOrWhiteSpace(result)
+            ? result
+            : null;
+    }
+
+    public static string GetRequiredGlobalOption(
+        this AnalyzerConfigOptionsProvider provider,
+        string name)
+    {
+        return
+            provider.GetGlobalOption(name) ??
+            throw new InvalidOperationException($"{name} is required.");
+    }
+
+    public static string GetRequiredOption(
+        this AnalyzerConfigOptionsProvider provider,
+        AdditionalText text,
+        string name)
+    {
+        return
+            provider.GetOption(text, name) ??
+            throw new InvalidOperationException($"{name} is required.");
+    }
+}
---
+++ src/libs/H.Generators.Extensions/SourceProductionContextExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace H.Generators.Extensions;
+
+public static class SourceProductionContextExtensions
+{
+    public static void AddTextSource(
+        this SourceProductionContext context,
+        string hintName,
+        string text,
+        Encoding? encoding = null)
+    {
+        hintName = hintName ?? throw new ArgumentNullException(nameof(hintName));
+        text = text ?? throw new ArgumentNullException(nameof(text));
+
+        context.AddSource(
+            hintName,
+            SourceText.From(
+                text,
+                encoding ?? Encoding.UTF8));
+    }
+
+    public static void ReportException(
+        this SourceProductionContext context,
+        string id,
+        Exception exception)
+    {
+        id = id ?? throw new ArgumentNullException(nameof(id));
+        exception = exception ?? throw new ArgumentNullException(nameof(exception));
+
+        context.ReportDiagnostic(
+            Diagnostic.Create(
+                new DiagnosticDescriptor(
+                    id,
+                    "Exception: ",
+                    $"{exception}",
+                    "Usage",
+                    DiagnosticSeverity.Error,
+                    true),
+                Location.None));
+    }
+}
