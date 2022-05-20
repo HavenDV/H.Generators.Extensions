1e66c0e4c3024b68753a574151a19fb92749eec1
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-05-20 23:51:19 +1000

    feat: Added EnumerableExtensions.

---
+++ src/libs/H.Generators.Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace H.Generators;
+
+/// <summary>
+/// 
+/// </summary>
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// Concatenates strings and cleans up line breaks at the beginning and end of the resulting string.
+    /// </summary>
+    /// <param name="values"></param>
+    /// <returns></returns>
+    public static string Inject(this IEnumerable<string> values)
+    {
+        return string.Concat(values)
+            .TrimStart('\r', '\n')
+            .TrimEnd('\r', '\n');
+    }
+}
