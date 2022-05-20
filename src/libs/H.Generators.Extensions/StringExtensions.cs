ad85adfae2af8a290752539dc50d4292fd852fe5
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-05-20 23:51:34 +1000

    feat: Added StringExtensions.

---
+++ src/libs/H.Generators.Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace H.Generators;
+
+/// <summary>
+/// 
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Makes the first letter of the name uppercase.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="ArgumentException"></exception>
+    public static string ToPropertyName(this string input)
+    {
+        return input switch
+        {
+            null => throw new ArgumentNullException(nameof(input)),
+            "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+            _ => input[0].ToString().ToUpper() + input.Substring(1),
+        };
+    }
+}
