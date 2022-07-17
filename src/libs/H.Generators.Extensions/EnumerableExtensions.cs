f14fc92f49954ce54eeeed4c42dea3069d8f8588
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-07-17 18:50:30 +1000

    fix: EnumerableExtensions.Inject will return " " for empty collections.

--- src/libs/H.Generators.Extensions/EnumerableExtensions.cs
+++ src/libs/H.Generators.Extensions/EnumerableExtensions.cs
@@ -6,14 +6,21 @@
 public static class EnumerableExtensions
 {
     /// <summary>
-    /// Concatenates strings and cleans up line breaks at the beginning and end of the resulting string.
+    /// Concatenates strings and cleans up line breaks at the beginning and end of the resulting string. <br/>
+    /// Returns " " if collection is empty(to use with <see cref="StringExtensions.RemoveBlankLinesWhereOnlyWhitespaces(string)"/>).
     /// </summary>
     /// <param name="values"></param>
     /// <returns></returns>
     public static string Inject(this IEnumerable<string> values)
     {
-        return string.Concat(values)
+        var text = string.Concat(values)
             .TrimStart('\r', '\n')
             .TrimEnd('\r', '\n');
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return " ";
+        }
+
+        return text;
     }
 }
