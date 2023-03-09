0b08e44ecba3ddac6438fa88062bc42fb139b4ba
Konstantin S <havendv@gmail.com>
2023-03-09 18:09:55 +0400

    fix: Added IncrementalValueProvider SelectAndReportExceptions.

--- src/libs/H.Generators.Extensions/IncrementalValuesProviderExtensions.cs
+++ src/libs/H.Generators.Extensions/IncrementalValuesProviderExtensions.cs
@@ -93,6 +93,52 @@ public static class IncrementalValuesProviderExtensions
     /// <typeparam name="TSource"></typeparam>
     /// <typeparam name="TResult"></typeparam>
     /// <returns></returns>
+    public static IncrementalValueProvider<TResult> SelectAndReportExceptions<TSource, TResult>(
+        this IncrementalValueProvider<TSource> source,
+        Func<TSource, CancellationToken, TResult> selector,
+        IncrementalGeneratorInitializationContext initializationContext,
+        string id = "SRE001")
+    {
+        var outputWithErrors = source
+            .Select<TSource, (TResult? Value, Exception? Exception)>((value, cancellationToken) =>
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                
+                try
+                {
+                    return (Value: selector(value, cancellationToken), Exception: null);
+                }
+                catch (Exception exception)
+                {
+                    return (Value: default, Exception: exception);
+                }
+            });
+
+        initializationContext.RegisterSourceOutput(outputWithErrors,
+            (context, tuple) =>
+            {
+                if (tuple.Exception is null)
+                {
+                    return;
+                }
+                
+                context.ReportException(id: id, exception: tuple.Exception);
+            });
+        
+        return outputWithErrors
+            .Select(static (x, _) => x.Value!);
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="selector"></param>
+    /// <param name="initializationContext"></param>
+    /// <param name="id"></param>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <returns></returns>
     public static IncrementalValuesProvider<TResult> SelectAndReportExceptions<TSource, TResult>(
         this IncrementalValuesProvider<TSource> source,
         Func<TSource, CancellationToken, TResult> selector,
